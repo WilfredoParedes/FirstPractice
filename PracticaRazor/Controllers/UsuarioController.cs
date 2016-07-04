a06e4e57e943da451b74ba7d373dbc54dabdd11c
WilfredoParedes <wilfredo.positiva@gmail.com>
2016-07-03 21:19:06 -0500

    Mi primer commit

---
+++ PracticaRazor/Controllers/UsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using PracticaRazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticaRazor.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        Prueba1Entities db = new Prueba1Entities();
+
+        public ActionResult Index()
+        {
+            
+            return View(db.Usuarios.ToList());
+        }
+	}
+}
---
+++ PracticaRazor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PracticaRazor.MvcApplication" Language="C#" %>
